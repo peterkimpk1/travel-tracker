c85e6a6cc08d00ca18a5ebba538268bbb77fac2a
Peter Kim <peterkim.pk1@gmail.com>
2024-06-05 17:23:42 -0400

    Created more data points for multiple userIDs

--- src/test-data/sample-trips.js
+++ src/test-data/sample-trips.js
@@ -179,7 +179,6 @@ export default { trips: [
         "status": "approved",
         "suggestedActivities": []
     },
-
     {
         "id": 19,
         "userID": 11,
@@ -199,5 +198,155 @@ export default { trips: [
         "duration": 13,
         "status": "approved",
         "suggestedActivities": []
-    }
+    },
+    {
+        "id": 21,
+        "userID": 21,
+        "destinationID": 29,
+        "travelers": 4,
+        "date": "2023/11/29",
+        "duration": 11,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 22,
+        "userID": 3,
+        "destinationID": 20,
+        "travelers": 3,
+        "date": "2023/07/02",
+        "duration": 14,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 23,
+        "userID": 22,
+        "destinationID": 45,
+        "travelers": 2,
+        "date": "2023/06/21",
+        "duration": 7,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 24,
+        "userID": 25,
+        "destinationID": 42,
+        "travelers": 5,
+        "date": "2023/05/06",
+        "duration": 12,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 25,
+        "userID": 16,
+        "destinationID": 50,
+        "travelers": 1,
+        "date": "2023/04/28",
+        "duration": 15,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 26,
+        "userID": 36,
+        "destinationID": 23,
+        "travelers": 3,
+        "date": "2023/10/12",
+        "duration": 6,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 27,
+        "userID": 15,
+        "destinationID": 12,
+        "travelers": 6,
+        "date": "2023/09/10",
+        "duration": 13,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 28,
+        "userID": 1,
+        "destinationID": 16,
+        "travelers": 2,
+        "date": "2023/03/26",
+        "duration": 8,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 29,
+        "userID": 11,
+        "destinationID": 27,
+        "travelers": 4,
+        "date": "2023/08/15",
+        "duration": 9,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 30,
+        "userID": 45,
+        "destinationID": 18,
+        "travelers": 5,
+        "date": "2023/01/22",
+        "duration": 10,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 31,
+        "userID": 29,
+        "destinationID": 33,
+        "travelers": 1,
+        "date": "2023/07/19",
+        "duration": 11,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 32,
+        "userID": 34,
+        "destinationID": 37,
+        "travelers": 6,
+        "date": "2023/12/14",
+        "duration": 8,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 33,
+        "userID": 7,
+        "destinationID": 38,
+        "travelers": 3,
+        "date": "2023/02/06",
+        "duration": 14,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 34,
+        "userID": 40,
+        "destinationID": 34,
+        "travelers": 5,
+        "date": "2023/11/03",
+        "duration": 7,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 35,
+        "userID": 12,
+        "destinationID": 21,
+        "travelers": 2,
+        "date": "2023/06/06",
+        "duration": 8,
+        "status": "approved",
+        "suggestedActivities": []
+      },
 ]}
