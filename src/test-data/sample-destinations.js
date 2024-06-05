aabca120c0c509fffbc7ed0a680c50ef21169e8e
Peter Kim <peterkim.pk1@gmail.com>
2024-06-05 16:52:29 -0400

    Created sample destinations test data

---
+++ src/test-data/sample-destinations.js
@@ -0,0 +1,156 @@
+export default { destinations: [
+    {
+        "id": 12,
+        "destination": "Miami, Florida",
+        "estimatedLodgingCostPerDay": 158,
+        "estimatedFlightCostPerPerson": 275,
+        "image": "https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80",
+        "alt": "sand with palm trees and tall buildings in the background"
+    },
+    {
+        "id": 15,
+        "destination": "Tulum, Mexico",
+        "estimatedLodgingCostPerDay": 100,
+        "estimatedFlightCostPerPerson": 350,
+        "image": "https://images.unsplash.com/photo-1501619593928-bef49688c888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "a donkey standing on the beach"
+    },
+    {
+        "id": 16,
+        "destination": "Rome, Italy",
+        "estimatedLodgingCostPerDay": 90,
+        "estimatedFlightCostPerPerson": 650,
+        "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "people standing inside a colosseum during the day"
+    },
+    {
+        "id": 18,
+        "destination": "Copenhagen, Denmark",
+        "estimatedLodgingCostPerDay": 120,
+        "estimatedFlightCostPerPerson": 1000,
+        "image": "https://images.unsplash.com/photo-1513622470522-26c3c8a854bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "colorful buildings with holiday decorations by the water with tents and docked boats"
+    },
+    {
+        "id": 20,
+        "destination": "Vilnius, Lithuania",
+        "estimatedLodgingCostPerDay": 65,
+        "estimatedFlightCostPerPerson": 1100,
+        "image": "https://images.unsplash.com/photo-1549891472-991e6bc75d1e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+        "alt": "overhead view of a city with a hot air balloon"
+    },
+    {
+        "id": 21,
+        "destination": "New York, New York",
+        "estimatedLodgingCostPerDay": 175,
+        "estimatedFlightCostPerPerson": 200,
+        "image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
+    },
+    {
+        "id": 23,
+        "destination": "London, England",
+        "estimatedLodgingCostPerDay": 100,
+        "estimatedFlightCostPerPerson": 1000,
+        "image": "https://images.unsplash.com/photo-1549471156-52ee71691122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "city with bridge under night sky"
+    },
+    {
+        "id": 26,
+        "destination": "San Francisco, California",
+        "estimatedLodgingCostPerDay": 175,
+        "estimatedFlightCostPerPerson": 200,
+        "image": "https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "golden gate bridge during the day time"
+    },
+    {
+        "id": 27,
+        "destination": "San Juan, Puerto Rico",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 900,
+        "image": "https://images.unsplash.com/photo-1580237541049-2d715a09486e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2090&q=80",
+        "alt": "white and brown concrete buildings near sea under white clouds during daytime"
+    },
+    {
+        "id": 29,
+        "destination": "Willemstad, Curaçao",
+        "estimatedLodgingCostPerDay": 80,
+        "estimatedFlightCostPerPerson": 1100,
+        "image": "https://images.unsplash.com/photo-1541748603027-cbfefa3a6c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
+        "alt": "brightly colored buildings near body of water"
+    },
+    {
+        "id": 33,
+        "destination": "Antananarivo, Madagascar",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 1200,
+        "image": "https://images.unsplash.com/photo-1563656353898-febc9270a0f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "landscape photo of a city skyline"
+    },
+    {
+        "id": 34,
+        "destination": "Colombo, Sri Lanka",
+        "estimatedLodgingCostPerDay": 55,
+        "estimatedFlightCostPerPerson": 1300,
+        "image": "https://images.unsplash.com/photo-1578159802020-13ec49d669df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "people walking inside flea market"
+    },
+    {
+        "id": 37,
+        "destination": "Kathmandu, Nepal",
+        "estimatedLodgingCostPerDay": 45,
+        "estimatedFlightCostPerPerson": 1200,
+        "image": "https://images.unsplash.com/photo-1558799401-1dcba79834c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+        "alt": "temple with buntings during daytime"
+    },
+    {
+        "id": 38,
+        "destination": "Brussels, Belgium",
+        "estimatedLodgingCostPerDay": 1000,
+        "estimatedFlightCostPerPerson": 110,
+        "image": "https://images.unsplash.com/photo-1559113202-c916b8e44373?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "brown concrete gate"
+    },
+    {
+        "id": 41,
+        "destination": "Seoul, South Korea",
+        "estimatedLodgingCostPerDay": 1200,
+        "estimatedFlightCostPerPerson": 150,
+        "image": "https://images.unsplash.com/photo-1578193661644-dee2e67b779b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2025&q=80",
+        "alt": "aerial photography of city during daytime"
+    },
+    {
+        "id": 42,
+        "destination": "Anchorage, Alaska",
+        "estimatedLodgingCostPerDay": 200,
+        "estimatedFlightCostPerPerson": 100,
+        "image": "https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "man riding on kayak surrounded by mountains"
+    },
+    {
+        "id": 45,
+        "destination": "Reykjavík, Iceland",
+        "estimatedLodgingCostPerDay": 900,
+        "estimatedFlightCostPerPerson": 120,
+        "image": "https://images.unsplash.com/photo-1515005319369-c4406c3f832b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+        "alt": "frozen river in the middle of rock mountains"
+    },
+    {
+        "id": 47,
+        "destination": "Frankfurt, Germany",
+        "estimatedLodgingCostPerDay": 1100,
+        "estimatedFlightCostPerPerson": 150,
+        "image": "https://images.unsplash.com/photo-1564859117892-8c3657465bfb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
+        "alt": "grey concrete bridge at night"
+    },
+    {
+        "id": 50,
+        "destination": "Helsinki, Finland",
+        "estimatedLodgingCostPerDay": 950,
+        "estimatedFlightCostPerPerson": 100,
+        "image": "https://images.unsplash.com/photo-1517128581046-8ee7e6fa3cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1484&q=80",
+        "alt": "two white motorboats on dock near brown trees at daytime"
+    }
+]
+ 
+}
