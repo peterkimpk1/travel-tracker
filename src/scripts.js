f9f2da91cf582a77a101cceabb3ed198edf2b30b
Peter Kim <peterkim.pk1@gmail.com>
2024-06-05 18:15:30 -0400

    Function getPastUserTrips passes both tests

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,34 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+const calculateTripCost = (tripData, destinationData, userID) =>  {
+    let totals = tripData.filter(trip => trip.userID === userID).reduce((acc,trip) => {
+        if (!acc.totalLodgingCost && !acc.totalFlightCost) {
+            acc.totalLodgingCost = 0;
+            acc.totalFlightCost = 0;
+        }
+        destinationData.forEach(destination => {
+            if(trip.destinationID === destination.id) {
+                acc.totalLodgingCost += (destination.estimatedLodgingCostPerDay) / (trip.travelers) * 1.1
+                acc.totalFlightCost += destination.estimatedFlightCostPerPerson * 1.1
+            }
+        })
 
+        return acc;
+    },{})
+    totals.totalLodgingCost = +totals.totalLodgingCost.toFixed(2)
+    totals.totalFlightCost = +totals.totalFlightCost.toFixed(2)
+    return totals 
+}
+const getPastUserTrips = (tripData, userID) => {
+   return tripData.reduce((userTrips, trip) => {
+        if(trip.userID === userID) {
+            userTrips.push(trip)
+        }
+        return userTrips
+    },[])
+}
 
-console.log('This is the JavaScript entry file - your code begins here.');
+export {
+    calculateTripCost,
+    getPastUserTrips
+}
