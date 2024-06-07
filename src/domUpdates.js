ab8c832a9daa00a92d6c9cd012d833109c53d56b
Peter Kim <peterkim.pk1@gmail.com>
2024-06-07 18:06:54 -0400

    Created function to getFormData when user clicks see estimate button. Created function to post trip estimate that calculates estimated cost based on form input and shows on DOM. Created function to make a POST request based on the form information.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,10 @@
 import {getRandomUser, calculatePastTripCosts, getPastUserTrips, getVisitedDestinationNames, getNonVisitedDestinationIDs, 
-    getDestinationInfo, calculateTripCost} from './userFunctions.js'
+    getDestinationInfo, calculateTotalTripCost, findLastTripId, findDestinationInfo} from './userFunctions.js'
 import Glide from '@glidejs/glide';
 
+let currentTripId;
 let currentUserId;
+let singleDestinationInfo;
 const welcomeHeader = document.querySelector('.welcome-message')
 const userCosts = document.querySelector('.user-total-costs');
 const userPastTrips = document.querySelector('.user-past-trips');
@@ -15,11 +17,15 @@ const modalSlides = document.querySelector('.glide__slides')
 const destinationSelection = document.getElementById('destinations')
 const tripDate = document.querySelector('#trip-date')
 const bookTripBtn = document.getElementById('book-trip-btn')
-
-
+const totalEstimateLine = document.getElementById('total-line')
+const totalEstimate = document.querySelector('.total-estimate')
+const breakdownSection = document.querySelector('.breakdown')
+const estimatedLodgeCost = document.querySelector('.estimate-lodge-cost')
+const estimatedFlightCost =  document.querySelector('.estimate-flight-cost')
 window.addEventListener('load',() => {
     fetchUserData()
     setTripDate()
+    toggleTotalEstimate('hidden')
 })
 
 openModalBtn.addEventListener('click',() =>{
@@ -32,25 +38,26 @@ openModalBtn.addEventListener('click',() =>{
 closeModalBtn.addEventListener('click', () => {
     modal.close()
 })
+bookTripBtn.addEventListener('click',() => {
+    postTripRequest()
+    modal.close()
+})
+
 bookTripForm.addEventListener('submit', (e) => {
     e.preventDefault()
     postTripEstimate()
 })
+
 const APICall = (urlEndPoint, options) => {
     return fetch(`http://localhost:3001/api/v1/${urlEndPoint}`,options).then(response => response.json())
  }
 
 const fetchUserData = () => {
-    const options = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-    Promise.all([APICall('travelers',options), APICall('trips',options),APICall('destinations',options)])
+    Promise.all([APICall('travelers'), APICall('trips'),APICall('destinations')])
     .then(e => {
        let user = getRandomUser(e[0].travelers)
-       currentUserId = user.id
+       currentTripId = findLastTripId(e[1].trips);
+       currentUserId = user.id;
        const allDestinationIDs = getNonVisitedDestinationIDs(e[1].trips,0)
        const allDestinationInfo = getDestinationInfo(e[2].destinations,allDestinationIDs)
        createGlideSlides(allDestinationInfo)
@@ -59,13 +66,11 @@ const fetchUserData = () => {
        const userTrips = getVisitedDestinationNames(e[1].trips,e[2].destinations,user.id)
        const sortedUserDates = getPastUserTrips(e[1].trips,user.id).map(trip => new Date(trip.date)).sort((a,b) => a-b);
        inputWelcomeMessage(user)
-       inputLastTripDate(sortedUserDates[0])
+       inputLastTripDate(sortedUserDates[sortedUserDates.length - 1])
        inputTotalCosts(userCost.totalFlightCost,userCost.totalLodgingCost)
        inputPastTrips(userTrips)
     }).catch(err => alert('Could not fetch user data..'))
 }
-
-
 
 const inputWelcomeMessage = (user) => {
     let firstName = user.name.split(' ')[0]
@@ -105,19 +110,62 @@ const createDestinationSelections = (destinations) => {
         destinationSelection.appendChild(option)
     })
 }
-
-const postTripEstimate = () => {
+const getFormData = () => {
     const formData = new FormData(bookTripForm)
     const destination = formData.get('destinations')
     const duration = formData.get('duration')
     const travelers = formData.get('travelers')
-    Promise.all([APICall('destinations')]).then(e => {
-        let totalCost = calculateTripCost(duration, travelers, destination, e[0].destinations)
-        console.log(totalCost)
-    })
-
+    const date = formData.get('date')
+    return {
+        destination: destination,
+        duration: duration,
+        travelers: travelers,
+        date: date
+    }
+}
+const postTripRequest = () => {
+    const formData = getFormData()
+    formData.date = formData.date.split("-").join("/")
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({
+            id: currentTripId + 1,
+            userID: currentUserId,
+            destinationID: singleDestinationInfo.id,
+            travelers: +formData.travelers,
+            date: formData.date,
+            duration: +formData.duration,
+            status: 'pending',
+            suggestedActivities: []
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+   APICall('trips',options).then(e => console.log(e)).catch(err => alert('did not work'))
 }
 
+const postTripEstimate = () => {
+    const formData = getFormData()
+    Promise.all([APICall('destinations')]).then(e => {
+        let totalCost = calculateTotalTripCost(formData.duration, formData.travelers, formData.destination, e[0].destinations)
+        toggleTotalEstimate('visible');
+        estimatedFlightCost.innerHTML = `Estimated Total Flight Cost <br>
+        <span>For ${formData.travelers} Adult(s): $${totalCost.flightCost}</span>`;
+        estimatedLodgeCost.innerHTML = `Estimated Total Lodging Cost <br>
+        <span>For ${formData.duration} Night(s): $${totalCost.lodgingCost}</span>`;
+        totalEstimate.innerHTML = `Total Estimate: 
+        <span class="dollar-estimate">$${totalCost.flightCost + totalCost.lodgingCost}
+        </span>`;
+        singleDestinationInfo = findDestinationInfo(e[0].destinations, formData.destination)
+    })
+}
+
+const toggleTotalEstimate = (property) => {
+    totalEstimateLine.style.visibility = property
+    totalEstimate.style.visibility = property
+    bookTripBtn.style.visibility = property
+}
 const setTripDate = () => {
     const date = new Date();
     let day = date.getDate();
